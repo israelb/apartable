c4a52a4a48cf84b6d8f4282e6b839ff2be36f040
Israel Barba <israel.barba@facturasi.com>
2014-08-30 12:43:57 -0500

    layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Apartamesa::Application.routes.draw do
-  get 'restaurantes', to: "restaurants#home"
+  get 'restaurantes', to: 'restaurants#home'
   get 'panel', to: 'restaurants#panel'
 
-  root :to => "home#index"
+  root to: 'home#index'
 
-  devise_for  :users, 
-              :path => '',
-              :path_names => {  :sign_in  => "login", 
-                                :sign_out => "logout", 
-                                :sign_up  => "register" },
-              controllers: {omniauth_callbacks: "omniauth_callbacks"}
-
+  devise_for  :users,
+              path: '',
+              path_names: { sign_in: 'login',
+                            sign_out: 'logout',
+                            sign_up: 'register' },
+              controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   scope 'restaurantes' do
-    devise_for  :restaurants, 
-                :path => '',
-                :path_names => {  :sign_in  => "login", 
-                                  :sign_out => "logout", 
-                                  :sign_up  => "register" }
+    devise_for  :restaurants,
+                path: '',
+                path_names: { sign_in: 'login',
+                              sign_out: 'logout',
+                              sign_up: 'register' }
   end
-
-
-
 end
